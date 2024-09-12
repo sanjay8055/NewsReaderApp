d2e9a1476c041243ebc9d9c4f7fedd73e40612f3
Sanjay Raskar <sanjayraskars@gmail.com>
2024-09-12 16:30:37 +0530

    Initial Commit

---
+++ NewsReaderApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsReaderApp
+//
+//  Created by Sanjay Raskar on 12/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsReaderApp/NewsReaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderAppApp.swift
+//  NewsReaderApp
+//
+//  Created by Sanjay Raskar on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsReaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
